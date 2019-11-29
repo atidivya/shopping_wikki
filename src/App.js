710912871b6d5b7410e702856a75c756a91af88e
atidivya <atidivya@gmail.com>
2019-11-29 14:22:45 +0000

    components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import Todos from './components/Todos';
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Ginger Garlic',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Besan',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Maggi',
+        completed: false
+      }
+    ]
+  }
   render() {
     return (
       <div className="App">
-       <h1>App</h1>
+       <Todos todos={this.state.todos} />
       </div>
     );
   }
